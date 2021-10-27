67edea82e55dd30ebe17af6b95152aee10904327
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-10-27 13:13:47 -0700

    test using the simple 'random dog image' api, it worked fine.  comment out the random number generator

--- src/components/Memegenerator.js
+++ src/components/Memegenerator.js
@@ -1,36 +1,46 @@
 import React, { useState, useEffect } from 'react'
-//import Memegenerator from "./components/Memegenerator"
 
 export default function Memegenerator() {
     const [topText, setTopText] = useState("One Does Not Simply")
-    const [bottomText, setBottomText] = useState("Learn React")
-    const [images, setImage] = useState("")
+    const [bottomText, setBottomText] = useState("Learn Hooks")
+    const [image, setImage] = useState("")
+
+// let randomNum;
+// let randomNumRoundDown;
+// let randomInteger;
+
+// function getRandomNum() {
+//     randomNum = Math.random() * 100;
+//     randomNumRoundDown = Math.floor(randomNum);
+//     randomInteger = Math.floor(randomNumRoundDown);
+//     return randomInteger;
+// }  
+
+const fetchMemes = function() {
+    fetch("https://api.imgflip.com/get_memes")
+    .then(response => response.json())
+    .then(data => 
+        //setImage(data.message)
+        console.log(data.data.memes[1].url)
+        )
+}   
 
 useEffect(() => {
-    fetch("https://api.imgflip.com/get_memes")
-        .then(response => response.json())
-        .then(data => setImage(data.data.memes))
+    fetchMemes()
 }, [])
-
-function handleClick() {
-    const randomNum = Math.random() * 100;
-    const randomNumRoundDown = Math.floor(randomNum);
-    console.log(Math.floor(randomNumRoundDown));
-    const newImage = images[randomNumRoundDown].url; 
-}  
 
     return (
         <div>
             <h1>{topText}</h1>
             <img
-                src={"https:i.imgflip.com/345v97.jpg"}
-                alt="random meme"
+                src={image ? image  : "https://i.imgflip.com/4t0m5.jpg"}
+                alt="random dog"
             ></img>
             <h1>{bottomText}</h1>
-            {console.log(images[11].url)}
+           
             <button
-                onClick={handleClick}
-            >Get New Meme!</button>
+                onClick={fetchMemes}
+            >Get New Meme!</button>  
         </div>
     )
 }
