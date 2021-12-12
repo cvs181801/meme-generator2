1499ac7a5415f2512d6fa26ef03394882de8b475
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-12-12 14:42:47 -0800

    add useEffect hook

--- src/components/Box.js
+++ src/components/Box.js
@@ -2,27 +2,33 @@ import React, {useState, useEffect} from 'react'
 
 export default function Box(props) {
 
-    const boxStyles = props.on ? {border: "1px solid red", padding: ".3em", color: "white", borderRadius: "7px", backgroundColor: "black", height: "5em", width: "5em"}
-    : {border: "1px solid black", padding: ".3em", color: "black", borderRadius: "7px", backgroundColor: "pink", height: "5em", width: "5em"}
+    //const boxStyles = props.on ? {border: "1px solid red", padding: ".3em", color: "white", borderRadius: "7px", backgroundColor: "black", height: "5em", width: "5em"}
+    //: {border: "1px solid black", padding: ".3em", color: "black", borderRadius: "7px", backgroundColor: "pink", height: "5em", width: "5em"}
 
     const [mode, setMode] = useState({id: props.id, on: props.on})
+    const [style, setStyle] = useState(mode ? {border: "1px solid red", padding: ".3em", color: "white", borderRadius: "7px", backgroundColor: "black", height: "5em", width: "5em"}
+    : {border: "1px solid black", padding: ".3em", color: "black", borderRadius: "7px", backgroundColor: "pink", height: "5em", width: "5em"})
     
 
     const handleClick = function() {
         console.log("clicked!!")
-        setMode(prevMode => console.log(prevMode))
-
+        setMode(prevMode => {
+            return {
+            ...prevMode,
+            on: !prevMode.on}})
+        console.log("Mode :", mode)
     }
 
-    // useEffect=>(()=> {
-    //     setMode(mode ? {border: "1px solid red", padding: ".3em", color: "white", borderRadius: "7px", backgroundColor: "black", height: "5em", width: "5em"}
-    // : {border: "1px solid black", padding: ".3em", color: "black", borderRadius: "7px", backgroundColor: "pink", height: "5em", width: "5em"}
-    //     )
-    // },[])
-    console.log(props)
+    useEffect(()=> {
+        setStyle(mode ? {border: "1px solid red", padding: ".3em", color: "white", borderRadius: "7px", backgroundColor: "black", height: "5em", width: "5em"}
+    : {border: "1px solid black", padding: ".3em", color: "black", borderRadius: "7px", backgroundColor: "pink", height: "5em", width: "5em"}
+        )
+    },[])
+
+    console.log("Props :", props)
     return (
         <div
-            style={boxStyles}
+            style={style}
             onClick={handleClick}
         >
          Hello! 
