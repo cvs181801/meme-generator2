4161c8e7dcfa341e40905385942cc3bee342e723
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-12-12 14:53:47 -0800

    add a way to dynamically change background color style upon clicking each box

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,12 +1,9 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 
 export default function Box(props) {
 
-    //const boxStyles = props.on ? {border: "1px solid red", padding: ".3em", color: "white", borderRadius: "7px", backgroundColor: "black", height: "5em", width: "5em"}
-    //: {border: "1px solid black", padding: ".3em", color: "black", borderRadius: "7px", backgroundColor: "pink", height: "5em", width: "5em"}
-
     const [mode, setMode] = useState({id: props.id, on: props.on})
-    const [style, setStyle] = useState(mode ? {border: "1px solid red", padding: ".3em", color: "white", borderRadius: "7px", backgroundColor: "black", height: "5em", width: "5em"}
+    const [style, setStyle] = useState(mode.on ? {border: "1px solid red", padding: ".3em", color: "white", borderRadius: "7px", backgroundColor: "black", height: "5em", width: "5em"}
     : {border: "1px solid black", padding: ".3em", color: "black", borderRadius: "7px", backgroundColor: "pink", height: "5em", width: "5em"})
     
 
@@ -17,15 +14,13 @@ export default function Box(props) {
             ...prevMode,
             on: !prevMode.on}})
         console.log("Mode :", mode)
+        setStyle(mode.on ? {border: "1px solid red", padding: ".3em", color: "white", borderRadius: "7px", backgroundColor: "black", height: "5em", width: "5em"}
+        : {border: "1px solid black", padding: ".3em", color: "black", borderRadius: "7px", backgroundColor: "pink", height: "5em", width: "5em"})
     }
 
-    useEffect(()=> {
-        setStyle(mode ? {border: "1px solid red", padding: ".3em", color: "white", borderRadius: "7px", backgroundColor: "black", height: "5em", width: "5em"}
-    : {border: "1px solid black", padding: ".3em", color: "black", borderRadius: "7px", backgroundColor: "pink", height: "5em", width: "5em"}
-        )
-    },[])
 
     console.log("Props :", props)
+    console.log("Mode.on :", mode.on)
     return (
         <div
             style={style}
