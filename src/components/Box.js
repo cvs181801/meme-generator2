9d671744dfc05e4a30cfeb3ca51de675e8039697
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-12-12 13:14:48 -0800

    add a dark mode using useState hook

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-export default function Header() {
+export default function Box() {
     return (
-        <div>
+        <div
             
+        >
+         Hello! 
         </div>
     )
 }
--- src/components/Memegenerator.js
+++ src/components/Memegenerator.js
@@ -1,29 +1,70 @@
 import React, { useState, useEffect } from 'react'
+import Box from './Box.js'
 
 export default function Memegenerator() {
     const [topText, setTopText] = useState("One Does Not Simply")
     const [bottomText, setBottomText] = useState("Learn Hooks")
     const [image, setImage] = useState("")
+    const [darkMode, setDarkMode] = useState(false)
+    const [boxesArray, setBoxesArray] = useState(
+    [
+    {
+        id: 1,
+        on: true
+    },   
+    {
+        id: 2,
+        on: false
+    },   
+    {
+        id: 3,
+        on: true
+    },   
+    {
+        id: 4,
+        on: true
+    },   
+    {
+        id: 5,
+        on: false
+    },   
+    {
+        id: 6,
+        on: false
+    },   
+])
 
-// let randomNum;
-// let randomNumRoundDown;
-// let randomInteger;
+const styles = 
+    darkMode ? {border: "1px solid red", borderRadius: "7px", backgroundColor: "black", height: "5em", width: "5em"}
+     : {border: "1px solid black", borderRadius: "7px", backgroundColor: "pink", height: "5em", width: "5em"}
 
-// function getRandomNum() {
-//     randomNum = Math.random() * 100;
-//     randomNumRoundDown = Math.floor(randomNum);
-//     randomInteger = Math.floor(randomNumRoundDown);
-//     return randomInteger;
-// }  
+
+let randomNum;
+let randomNumRoundDown;
+let randomInteger;
+
+const newArray = boxesArray.map(box => {
+    return <div key={box.id} style={styles}></div>
+})
+
+function getRandomNum() {
+    randomNum = Math.random() * 100;
+    randomNumRoundDown = Math.floor(randomNum);
+    randomInteger = Math.floor(randomNumRoundDown);
+    return randomInteger;
+}  
 
 const fetchMemes = function() {
     fetch("https://api.imgflip.com/get_memes")
-    .then(response => response.json())
-    .then(data => 
-        //setImage(data.message)
-        console.log(data.data.memes[1].url)
+        .then(response => response.json())
+        .then(data => 
+            setImage(data.data.memes[getRandomNum()].url)
         )
 }   
+
+const handleClick = function() {
+    setDarkMode(prevMode => !prevMode)
+}
 
 useEffect(() => {
     fetchMemes()
@@ -31,16 +72,22 @@ useEffect(() => {
 
     return (
         <div>
+            <button
+                onClick={handleClick}
+            ></button>
             <h1>{topText}</h1>
             <img
                 src={image ? image  : "https://i.imgflip.com/4t0m5.jpg"}
-                alt="random dog"
+                alt="random meme"
             ></img>
             <h1>{bottomText}</h1>
            
             <button
                 onClick={fetchMemes}
             >Get New Meme!</button>  
+
+            {newArray}
+         
         </div>
     )
 }
